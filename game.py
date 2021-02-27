5207bf3d3b405b211501d79fb1adb0c7c034a450
Kevin Huestis <kevin.huestis@tum.de>
2021-02-27 13:59:10 +0100

    GUI

--- game.py
+++ game.py
@@ -1,10 +1,5 @@
 import chess
 import chess.svg
-import time
-import threading
-
-from PyQt5.QtSvg import QSvgWidget
-from PyQt5.QtWidgets import QApplication, QWidget
 
 class Game():
 
@@ -17,7 +12,7 @@ class Game():
 
         if not self.white_next:
             raise Exception("Bruh it's not your turn")
-        
+
         if not move in self.board.legal_moves:
             raise Exception("Dumbass, that shit ain't legal")
 
@@ -27,10 +22,10 @@ class Game():
 
     def play_black(self, move_name):
         move = chess.Move.from_uci(move_name)
-        
+
         if self.white_next:
             raise Exception("Bruh it's not your turn")
-        
+
         if not move in self.board.legal_moves:
             raise Exception("Dumbass, that shit ain't legal")
 
@@ -40,38 +35,18 @@ class Game():
 
     def end_move(self):
 
-        suff = self.board.is_insufficient_material() 
+        suff = self.board.is_insufficient_material()
         stale = self.board.is_stalemate()
 
         if self.board.is_checkmate():
-            print("You won boooyyyyy!!")
+            print("You won boooiiiii!!")
             return False
-        elif suff or stale: # draw
+        elif suff or stale:  # draw
             print("issa draw bish...")
         elif self.board.is_game_over():
             raise Exception("The game ended for some reason...")
         else:
             return True
-
-class MainWindow(QWidget):
-    
-    def __init__(self, board):
-        super().__init__()
-
-        self.setGeometry(100, 100, 520, 520)
-
-        self.widgetSvg = QSvgWidget(parent=self)
-        self.widgetSvg.setGeometry(10, 10, 500, 500)
-
-        self.chessboard = board
-
-        self.chessboardSvg = chess.svg.board(self.chessboard).encode("UTF-8")
-        self.widgetSvg.load(self.chessboardSvg)
-
-    def paintEvent(self, event):
-        self.chessboardSvg = chess.svg.board(self.chessboard).encode("UTF-8")
-        self.widgetSvg.load(self.chessboardSvg)
-        self.show()
 
 
 if __name__ == '__main__':
@@ -79,16 +54,9 @@ if __name__ == '__main__':
     game = Game()
     mate_sequence = ['e2e4', 'e7e5', 'd1h5', 'e8e7', 'h5e5']
 
-    app = QApplication([])
-    window = MainWindow(game.board)
-    window.show()
-    threading.Thread(target=app.exec).start()
-
     for move in mate_sequence:
         if game.white_next:
             game.play_white(move)
         else:
             game.play_black(move)
 
-        window.paintEvent(True)
-        time.sleep(1)
